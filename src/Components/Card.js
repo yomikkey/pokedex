b1e23bf853428fe8190d4629b598d3623363847f
ANNUKUL RAWAT <anukulmickey9@gmail.com>
2021-10-22 17:23:42 +0530

    Please enter the commit message for your changes. Lines starting

--- src/Components/Card.js
+++ src/Components/Card.js
@@ -6,36 +6,58 @@ import CardContent from "@mui/material/CardContent";
 import CardMedia from "@mui/material/CardMedia";
 import Button from "@mui/material/Button";
 import Typography from "@mui/material/Typography";
+import axios from "axios";
 
 // function capitalizeFirstLetter(string) {
 //     return string.charAt(0).toUpperCase() + string.slice(1);
 //   }
 
 export default function MediaCard(props) {
-    const [name, setName] = useState(null);
-    const [img, setImg] = useState("");
+  // const [name, setName] = useState([]);
+  const axios = require("axios");
+  const [img, setImg] = useState('');
 
-    useEffect(() => {
-      setName(props.data.name);
-      // setImg(props.data.sprites.other["official-artwork"].front_default)
-      if (props.data.sprites.other["official-artwork"].front_default) {
-        setImg(props.data.sprites.other["official-artwork"].front_default);
-      } else {
-        setImg(
-          "https://upload.wikimedia.org/wikipedia/commons/d/d1/Image_not_available.png"
-        );
+  useEffect(() => {
+    getData()
+    return () => {};
+  },[]);
+
+  async function getData() {
+    try {
+      let res = await axios({
+        url: `https://pokeapi.co/api/v2/pokemon/${props.name}`,
+        method: "get"
+      });
+      if (res.status === 200) {
+        if (res.data.sprites.other["official-artwork"].front_default) {
+            setImg(
+              res.data.sprites.other["official-artwork"].front_default
+            );
+          } else {
+            setImg(
+              "https://upload.wikimedia.org/wikipedia/commons/d/d1/Image_not_available.png"
+            );
+          }
       }
-      return () => {};
-    });
-console.log(props);
+      return res.data;
+    } catch (err) {
+      console.error(err);
+    }
+  }
+
 
   return (
     <Card sx={{ maxWidth: 350 }}>
-      <CardMedia component="img" height="200" width="75" image={img}/>
+      <CardMedia
+        component="img"
+        height="200"
+        width="75"
+           image={img}
+      />
       <CardContent>
         <Typography gutterBottom variant="h5" component="div">
           {/* {capitalizeFirstLetter(props.data.name)} */}
-          {props.data.name}
+          {props.name}
         </Typography>
         <Typography variant="body2" color="text.secondary"></Typography>
       </CardContent>
--- src/Components/Pokemon.js
+++ src/Components/Pokemon.js
@@ -1,40 +1,48 @@
 import * as React from "react";
 import { useState, useEffect } from "react";
 import MediaCard from "./Card";
+import Grid from "@mui/material/Grid";
 
 export const Pokemon = () => {
-  const [pokemon_details, setPokemonDetails] = useState({});
+  const axios = require("axios");
+  const [pokemon_list, setPokemonList] = useState(null);
 
   useEffect(() => {
-    getData().then((res) => console.log("res"));
-    return () => {
-    }
-  }, [])
-
-  const axios = require("axios");
+    getData().then((res) =>
+      console.log(`Data Fetched Successfully `)
+    );
+    return () => {};
+  }, []);
 
   async function getData() {
     try {
       let res = await axios({
-        url: "https://pokeapi.co/api/v2/pokemon/pikachu",
-        method: "get",
-        timeout: 8000,
+        url: "https://pokeapi.co/api/v2/pokemon",
+        method: "get"
       });
       if (res.status === 200) {
-      
-        setPokemonDetails(res.data);
-        console.log(res.status);
+        setPokemonList(res.data.results);
+        // setPokemonDetails(res.data);
+        // console.log(pokemon_list);
+        // console.log(res.data)
       }
-     
+
       return res.data;
     } catch (err) {
-      console.error(err); 
+      console.error(err);
     }
   }
 
   return (
     <div>
-      <MediaCard data={pokemon_details} />
+      <Grid container spacing={5}>
+      {pokemon_list
+        ? pokemon_list.map((list) => {
+            return <MediaCard name={list.name} />;
+            // return console.log(name.name)
+          })
+        : null}
+        </Grid>
     </div>
   );
 };
