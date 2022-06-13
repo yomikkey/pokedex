daa714c6f9cc2bc05e5109009e51a9dc409a9f4b
ANUKUL RAWAT <anukulmickey9@gmail.com>
2022-06-13 23:18:21 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -8,6 +8,19 @@ import { Pokemon } from "./Components/Pokemon";
 function App() {
   return (
     <div className="App">
+    
+            <meta property="og:site_name" content="Pokemon-Inventory" />
+            <meta property='og:title' content='PokeDex App' />
+            <meta property='og:image' content='https://ibb.co/cvn8TpR' />
+            <meta property='og:description' content='Visual PokeDex App' />
+            <meta property='og:url' content='https://pokemon-inventory.netlify.app/' />
+            <meta property="og:type" content='website' />
+            <meta property='og:image:width' content='1200' />
+            <meta property='og:image:height' content='627' />
+            {/* <!-- TYPE BELOW IS PROBABLY: 'website' or 'article' or look on https://ogp.me/#types --> */}
+            <meta property="og:locale" content="en_US" />
+            <meta property="og:locale:alternate" content="en_US" />
+              
       <div className="Navbar">
         <Navbar />
       </div>
